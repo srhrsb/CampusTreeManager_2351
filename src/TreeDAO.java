a03356bc9b83c70cf31e8401d32e065c09bf2f27
srhrsb <sebastian.boettcher2@srh.de>
2025-06-26 11:59:16 +0200

    Stand nach 17UE

--- src/TreeDAO.java
+++ src/TreeDAO.java
@@ -4,6 +4,23 @@ public class TreeDAO {
 
     private ArrayList<Tree> treeList;
     public TreeDAO() {
-           treeList = new ArrayList<>();
+        treeList = new ArrayList<>();
     }
+
+    public boolean addTree( int id, String name, double lng, double lat, String type, boolean isSick )
+    {
+        //Aufgabe b:
+
+        //Schritt 1: Objekt von Tree mit den Daten erzeugen
+        Tree tree = new Tree(id, name, lng, lat, type, isSick);
+
+        //Schritt 2: erzeugtes Objekt zuweisen
+        treeList.add(tree);
+
+        //kürzer
+        //treeList.add( new Tree(id, name, lng, lat, type, isSick) );
+
+        return false;
+    }
+
 }
